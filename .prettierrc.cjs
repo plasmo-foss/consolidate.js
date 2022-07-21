e636bbe9cba11db0165affe4a223c25f1541ec59
Louis <6723574+louisgv@users.noreply.github.com>
2022-07-21 13:09:29 -0400

    Bump dependencies, remove old stuffs

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import('prettier').Options}
+ */
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: false,
+  bracketSameLine: true,
+  singleAttributePerLine: true
+};
